8daafb60cb515e5ca9e5909a0b0e49e7644f45f1
shymaSane <shymaSane1@gmail.com>
2017-11-11 20:35:02 +0200

    tried none on view encapsulation

--- view-enc/src/app/hello/hello.component.ts
+++ view-enc/src/app/hello/hello.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  styleUrls: ['./hello.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HelloComponent implements OnInit {
 
